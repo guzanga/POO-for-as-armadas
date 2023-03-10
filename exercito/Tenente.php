d8d58aef42e2a1f4564eb6c47bb3d99612250e47
basqueoro <basqueroto.felipe125@gmail.com>
2023-03-10 10:07:24 -0300

    criação da classe tenente

---
+++ exercito/Tenente.php
@@ -0,0 +1,11 @@
+<?php
+
+class Tenente extends Exercito {
+    public function __construct($nome, $patente, $arma, $merito_militar) {
+        parent::__construct($nome, $patente, $arma, $merito_militar);
+    }
+
+    public function grito_of_war() {
+        echo "Se eu pudesse juntar...minha vida nao seria tao dificil, mas quem disse que a vida eh facil?!";
+    }
+}
